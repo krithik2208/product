34695fa0c3b57abda0837b5b4c04f3b93a25997a
I587673 <krithik.r@sap.com>
2026-04-03 14:47:04 +0530

    Fix: detect product page from URL pathname instead of query param

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import Product1Page from './pages/Product1Page'
 import Product2Page from './pages/Product2Page'
 import Product3Page from './pages/Product3Page'
 
-const params = new URLSearchParams(window.location.search)
-const page = params.get('page')
+const path = window.location.pathname
 
 function App() {
-  if (page === '2') {
+  if (path.includes('product2')) {
     return <Product2Page />
   }
-  if (page === '3') {
+  if (path.includes('product3')) {
     return <Product3Page />
   }
   return <Product1Page />
